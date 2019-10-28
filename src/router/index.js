8e338a2a0212e2cf6e1cbaaf2d8ee8ea3f137583
No979545579 <979545579@qq.com>
2019-10-28 11:46:14 +0800

    404页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 路由配置对象
@@ -17,6 +18,9 @@ const router = new VueRouter({
   }, {
     path: '/login',
     component: Login
+  }, {
+    path: '*',
+    component: NotFound
   }]
 })
 export default router
